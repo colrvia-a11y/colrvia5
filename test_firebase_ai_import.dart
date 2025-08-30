212766d2454408943962a6822393cee2070a7417
Corey Hamilton <colrvia@gmail.com>
2025-08-30 18:07:52 -0500

    feat: Update Firebase configuration and enhance logging in test script

--- test_firebase_ai_import.dart
+++ test_firebase_ai_import.dart
@@ -1,5 +1,8 @@
 
+import 'package:logging/logging.dart';
+
 void main() {
+  final logger = Logger('FirebaseAIImportTest');
   // Test to see what's available in the package
-  print('Testing Firebase AI import');
+  logger.info('Testing Firebase AI import');
 }
